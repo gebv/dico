b2f7d39ba7972e990833f102a8d64c5194365055
G B <egor.workroom@gmail.com>
2016-04-11 16:37:12 +0600

    обновлен пример

--- examples/main.go
+++ examples/main.go
@@ -8,13 +8,17 @@
 //[struct]
 // name ="FooBarStruct"
 // [[struct.fields]]
-// comment = "comment"
-// name = "Foo"
-// type = "string"
-// tag = '''json:"Foo"'''
+// name = "M"
+// type = "map[string]interface{}"
 // [[struct.fields]]
-// name = "Bar"
+// name = "I"
+// type = "int"
+// [[struct.fields]]
+// name = "S"
 // type = "string"
+// [[struct.fields]]
+// name = "A"
+// type = "[]string"
 //config.toml
 //AUTOGENERATE.DICO>>>
 //	The text in the section 'AUTOGENERATE.DICO' automatically generated, please do not edit it
@@ -34,44 +38,162 @@ var ErrNotAllowed = "ErrNotAllowed"
 
 
 
+func NewFooBarStruct() *FooBarStruct {
+    model := new(FooBarStruct)
+    
+    
+    model.M = make(map[string]interface{})
+      
+    
+      
+    
+      
+    
+      
+    
+    return model
+}
 type FooBarStruct struct {
     
-    // comment
-    Foo string `json:"Foo"`  
+    
+    M map[string]interface{}   
     
     
-    Bar string   
+    I int   
+    
+    
+    S string   
+    
+    
+    A []string   
     
 }
 
-// SetFoo set Foo
-func (f *FooBarStruct) SetFoo(v string) {
-    f.Foo = v
-}
- 
 
-// GetFoo get Foo
-func (f *FooBarStruct) GetFoo() string {
-    return f.Foo
-}
- 
 
-// SetBar set Bar
-func (f *FooBarStruct) SetBar(v string) {
-    f.Bar = v
-}
- 
 
-// GetBar get Bar
-func (f *FooBarStruct) GetBar() string {
-    return f.Bar
+
+// SetM set all elements M
+func (f *FooBarStruct) SetM(v map[string]interface{}) {
+    f.M = make(map[string]interface{})
+    
+    for key, value := range v {
+        f.M[key] = value
+    }
 }
- 
+
+// AddM add element by key
+func (f *FooBarStruct) SetOneM(k string, v interface{}) {
+    f.M[k] = v
+}
+
+// RemoveM remove element by key
+func (f *FooBarStruct) RemoveM(k string) {
+    if _, exist := f.M[k]; exist {
+        delete(f.M, k)  
+    } 
+}
+
+
+
+
+// GetM get M
+func (f *FooBarStruct) GetM() map[string]interface{} {
+    return f.M
+}
+
+
+
+
+// ExistM has exist key M
+func (f *FooBarStruct) ExistKeyM(k string) bool {
+     _, exist := f.M[k]
+     
+     return exist
+}
+
+func (f *FooBarStruct) GetOneM(k string) interface{} {
+    return f.M[k]
+}
+
+
+
+// SetI set I
+func (f *FooBarStruct) SetI(v int) {
+    f.I = v
+}
+
+
+
+
+// GetI get I
+func (f *FooBarStruct) GetI() int {
+    return f.I
+}
+
+
+// SetS set S
+func (f *FooBarStruct) SetS(v string) {
+    f.S = v
+}
+
+
+
+
+// GetS get S
+func (f *FooBarStruct) GetS() string {
+    return f.S
+}
+
+
+// AddA add element A
+func (f *FooBarStruct) AddA(v string) {
+    if f.IncludeA() {
+        return
+    }
+    
+    f.A = append(f.A, v)
+}
+
+// RemoveA remove element A
+func (f *FooBarStruct) RemoveA(v string) {
+    if !f.IncludeA(v) {
+        return
+    }
+    
+    _i := f.IndexA(v)
+    
+    f.A = append(f.A[:_i], f.A[_i+1:]...)
+}
+
+
+
+
+// GetA get A
+func (f *FooBarStruct) GetA() []string {
+    return f.A
+}
+// IndexA get index element A
+func (f *FooBarStruct) IndexA(v string) int {
+    for _index, _v := range f.A {
+        if _v == v {
+            return _index
+        }
+    }
+    return -1
+}
+
+// IncludeA has exist value A
+func (f *FooBarStruct) IncludeA(v string) bool {
+    return f.IndexA(v) > -1
+}
+
+
 
 
 
 func main() {
-    s := &FooBarStruct{"foo", "bar"}
+    s := NewFooBarStruct()
     fmt.Printf("%v", s)
 }
 
