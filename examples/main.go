265540a85a79c58f82f39b0aecb1967be93959b3
G B <egor.workroom@gmail.com>
2016-04-11 18:54:50 +0600

    обновлен пример

--- examples/main.go
+++ examples/main.go
@@ -1,4 +1,4 @@
-//dico main templates/golang/*
+//dico main
 //config.toml
 //pkg = "main"
 // errors = ["ErrNotFound", "err not allowed"]
@@ -23,179 +23,135 @@
 //AUTOGENERATE.DICO>>>
 //	The text in the section 'AUTOGENERATE.DICO' automatically generated, please do not edit it
 //[DICO.VERSION]:	 0.0.2
-//[DICO.COMMAND]:	  main templates/golang/*
+//[DICO.COMMAND]:	  main
 
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
-
 
 var ErrNotFound = "ErrNotFound"
 var ErrNotAllowed = "ErrNotAllowed"
- 
-
-
 
 func NewFooBarStruct() *FooBarStruct {
-    model := new(FooBarStruct)
-    
-    
-    model.M = make(map[string]interface{})
-      
-    
-      
-    
-      
-    
-      
-    
-    return model
+	model := new(FooBarStruct)
+
+	model.M = make(map[string]interface{})
+
+	return model
 }
+
 type FooBarStruct struct {
-    
-    
-    M map[string]interface{}   
-    
-    
-    I int   
-    
-    
-    S string   
-    
-    
-    A []string   
-    
+	M map[string]interface{}
+
+	I int
+
+	S string
+
+	A []string
 }
-
-
-
-
 
 // SetM set all elements M
 func (f *FooBarStruct) SetM(v map[string]interface{}) {
-    f.M = make(map[string]interface{})
-    
-    for key, value := range v {
-        f.M[key] = value
-    }
+	f.M = make(map[string]interface{})
+
+	for key, value := range v {
+		f.M[key] = value
+	}
 }
 
 // AddM add element by key
 func (f *FooBarStruct) SetOneM(k string, v interface{}) {
-    f.M[k] = v
+	f.M[k] = v
 }
 
 // RemoveM remove element by key
 func (f *FooBarStruct) RemoveM(k string) {
-    if _, exist := f.M[k]; exist {
-        delete(f.M, k)  
-    } 
+	if _, exist := f.M[k]; exist {
+		delete(f.M, k)
+	}
 }
-
-
-
 
 // GetM get M
 func (f *FooBarStruct) GetM() map[string]interface{} {
-    return f.M
+	return f.M
 }
-
-
-
 
 // ExistM has exist key M
 func (f *FooBarStruct) ExistKeyM(k string) bool {
-     _, exist := f.M[k]
-     
-     return exist
+	_, exist := f.M[k]
+
+	return exist
 }
 
 func (f *FooBarStruct) GetOneM(k string) interface{} {
-    return f.M[k]
+	return f.M[k]
 }
-
-
 
 // SetI set I
 func (f *FooBarStruct) SetI(v int) {
-    f.I = v
+	f.I = v
 }
-
-
-
 
 // GetI get I
 func (f *FooBarStruct) GetI() int {
-    return f.I
+	return f.I
 }
-
 
 // SetS set S
 func (f *FooBarStruct) SetS(v string) {
-    f.S = v
+	f.S = v
 }
-
-
-
 
 // GetS get S
 func (f *FooBarStruct) GetS() string {
-    return f.S
+	return f.S
 }
-
 
 // AddA add element A
 func (f *FooBarStruct) AddA(v string) {
-    if f.IncludeA() {
-        return
-    }
-    
-    f.A = append(f.A, v)
+	if f.IncludeA(v) {
+		return
+	}
+
+	f.A = append(f.A, v)
 }
 
 // RemoveA remove element A
 func (f *FooBarStruct) RemoveA(v string) {
-    if !f.IncludeA(v) {
-        return
-    }
-    
-    _i := f.IndexA(v)
-    
-    f.A = append(f.A[:_i], f.A[_i+1:]...)
+	if !f.IncludeA(v) {
+		return
+	}
+
+	_i := f.IndexA(v)
+
+	f.A = append(f.A[:_i], f.A[_i+1:]...)
 }
-
-
-
 
 // GetA get A
 func (f *FooBarStruct) GetA() []string {
-    return f.A
+	return f.A
 }
+
 // IndexA get index element A
 func (f *FooBarStruct) IndexA(v string) int {
-    for _index, _v := range f.A {
-        if _v == v {
-            return _index
-        }
-    }
-    return -1
+	for _index, _v := range f.A {
+		if _v == v {
+			return _index
+		}
+	}
+	return -1
 }
 
 // IncludeA has exist value A
 func (f *FooBarStruct) IncludeA(v string) bool {
-    return f.IndexA(v) > -1
+	return f.IndexA(v) > -1
 }
 
-
-
-
-
 func main() {
-    s := NewFooBarStruct()
-    fmt.Printf("%v", s)
+	s := NewFooBarStruct()
+	fmt.Printf("%v", s)
 }
 
 //<<<AUTOGENERATE.DICO
-
